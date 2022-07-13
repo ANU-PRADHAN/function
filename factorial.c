70efa98e8d2c877a3bb3f205361c01daccf0f550
ANU-PRADHAN <109081407+ANU-PRADHAN@users.noreply.github.com>
2022-07-13 10:00:05 +0545

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+long long int series(int);
+int main(){
+	int n;
+	long long int f;
+	printf("Enter a value for the nth term:\n");
+	scanf("%d",&n);
+	f= series(n);
+	printf("Factorial is %lld",f);
+	return 0;	
+}
+long long int series(int n){
+	if(n==0){
+		return 1;
+	}
+	else{
+		return(n*series(n-1));
+	}
+}
